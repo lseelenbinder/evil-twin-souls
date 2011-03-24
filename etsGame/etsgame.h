a92cc892a25c9d686f039d81fcada6dff090bfe2
luke.seelenbinder@gmail.com <luke.seelenbinder@gmail.com@b7444b13-40a3-a05b-aa3e-fda0021e588c>
2011-03-24 20:58:37 +0000

    Create partial header for Player.

--- etsGame/etsgame.h
+++ etsGame/etsgame.h
@@ -2,6 +2,8 @@
 #define ETSGAME_H
 
 #include <QMainWindow>
+#include <QLabel>
+#include <QPixmap>
 
 namespace Ui {
     class etsGame;
@@ -17,6 +19,13 @@ public:
 
 private:
     Ui::etsGame *ui;
+    QLabel player;
+    QPixmap playerSprite;
+    void movePlayer(int);
+
+private slots:
+    void onkeyPressEvent(QKeyEvent*);
+
 };
 
 #endif // ETSGAME_H
